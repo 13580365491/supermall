7d81cb52a4563a4a6a4abbe7ab0dee2c66af5ad2
tanmoruo <280445171@qq.com>
2020-11-15 19:51:38 +0800

    首页轮播图开发完成

---
+++ src/components/content/MainTabBar/MainTabBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <tabbar>
+      <tabbar-item path="/home">
+        <img src="~assets/img/tabbar/home.svg" alt="" slot="item-icon"/>
+        <img src="~assets/img/tabbar/home-active.svg" alt="" slot="item-icon-active"/>
+        <div slot="item-text">首页</div>
+      </tabbar-item>
+      <tabbar-item path="/category">
+        <img src="~assets/img/tabbar/category.svg" alt="" slot="item-icon"/>
+        <img src="~assets/img/tabbar/category-active.svg" alt="" slot="item-icon-active"/>
+        <div slot="item-text">分类</div>
+      </tabbar-item>
+      <tabbar-item path="/cart">
+        <img src="~assets/img/tabbar/shopcart.svg" alt="" slot="item-icon"/>
+        <img src="~assets/img/tabbar/shopcart-active.svg" alt="" slot="item-icon-active"/>
+        <div slot="item-text">购物车</div>
+      </tabbar-item>
+      <tabbar-item path="/bussiness">
+        <img src="~assets/img/tabbar/bussiness-man.svg" alt="" slot="item-icon"/>
+        <img src="~assets/img/tabbar/bussiness-man-active.svg" alt="" slot="item-icon-active"/>
+        <div slot="item-text">我的</div>
+      </tabbar-item>
+    </tabbar>
+</template>
+
+<script>
+import tabbar from "components/common/tabbar/tabbar";
+import tabbarItem from "components/common/tabbar/tabbarItem";
+
+export default {
+  components: {
+    tabbar,
+    tabbarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home/childComps/homeSwiper.vue
@@ -0,0 +1,31 @@
+<template>
+  <swiper>
+    <swiper-item v-for="(item, index) in banners" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import { swiper, swiperItem } from "components/common/swiper";
+
+export default {
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  components: {
+    swiper,
+    swiperItem,
+  },
+};
+</script>
+
+<style>
+</style>
